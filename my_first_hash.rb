e188940706c21d0207f8cdd40514b339bd98ab83
Jason Mei <jason.mei403@gmail.com>
2018-06-07 22:59:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,10 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  me_hash = {
+    "Google" => "www.google.com",
+    "YouTube" => "www.youtube.com",
+    "Facebook" => "www.facebook.com"
+  }
   
 end
 
